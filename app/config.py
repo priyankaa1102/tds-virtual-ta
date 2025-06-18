101a9b8f8c5a638ea6966fc64e9b216811ebba28
priyankaa1102 <94786629+priyankaa1102@users.noreply.github.com>
2025-06-18 14:04:29 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+# Path configuration
+DATA_PATH = Path(__file__).parent.parent / "data/tds_data.json"
+
+# API settings
+MAX_DISCOURSE_PAGES = 3
+COURSE_URL = "https://tds.s-anand.net/#/2025-01/"
